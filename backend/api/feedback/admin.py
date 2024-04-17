1892afe116156ed0b24f463c17c75e009c46cd5b
aprskalo1 <ante.prskalo4@gmail.com>
2024-04-17 23:26:03 +0200

    done authentication

--- backend/api/feedback/admin.py
+++ backend/api/feedback/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from feedback.models import Feedback, Question, Score
 
-# Register your models here.
+admin.site.register(Feedback)
+admin.site.register(Question)
+admin.site.register(Score)
